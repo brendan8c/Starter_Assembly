41332ec4430d88a12515fd24cee4ce8a08e8fe11
Brendan8c <brendan8c@gmail.com>
2023-03-03 20:05:57 +0300

    Stop tracking package-lock.json

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import cl from '../scss/styles/header.module.scss';
+import cn from 'classnames';
 
 const Header = () => {
   return (
@@ -7,7 +8,7 @@ const Header = () => {
         <img src="./src/assets/vite.svg" className={cl.logo} alt="Vite logo" />
       </a>
       <a href="https://reactjs.org" target="_blank">
-        <img src="./src/assets/react.svg" className={cl.logo} alt="React logo" />
+        <img src="./src/assets/react.svg" className={cn(cl.logo, cl.active)} alt="React logo" />
       </a>
       <a href="https://tailwindcss.com" target="_blank">
         <img src="./src/assets/tailwindcss.svg" className={cl.logo} alt="Tailwindcss logo" />
