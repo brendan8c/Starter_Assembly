537f97ed57bd83ddee38e73356268c1417742a40
Brendan8c <brendan8c@gmail.com>
2023-07-15 23:58:11 +0300

    Changed directory structures for ready assembly

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,29 @@ export default defineConfig({
     }),
   ],
   base: './',
+  build: {
+    outDir: 'dist/',
+    assetsDir: '',
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/[name]-[hash].js',
+        chunkFileNames: 'js/[name]-[hash].js',
+        assetFileNames: assetInfo => {
+          const info = assetInfo.name.split('.');
+          const extType = info[info.length - 1];
+          if (/\.(png|jpe?g|gif|svg|webp|webm|mp3)$/.test(assetInfo.name)) {
+            return `media/[name]-[hash].${extType}`;
+          }
+          if (/\.(css)$/.test(assetInfo.name)) {
+            return `css/[name]-[hash].${extType}`;
+          }
+          if (/\.(woff|woff2|eot|ttf|otf)$/.test(assetInfo.name)) {
+            return `fonts/[name]-[hash].${extType}`;
+          }
+          return `[name]-[hash].${extType}`;
+        },
+      },
+    },
+  },
 });
